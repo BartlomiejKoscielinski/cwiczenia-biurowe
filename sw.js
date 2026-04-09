2553ae80812c5430c0c21134bb7671a564396af7
Bartlomiej <bartlomiejkoscielinski@gmail.com>
2026-04-09 19:35:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cwiczenia-v2';
+const CACHE = 'cwiczenia-v3';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
